8cde7e5f4ab8931b19fc86928368487def2b9a9f
jon4t <jon4th4nrakotomalala@gmail.com>
2024-11-30 13:05:47 +0100

    Commit spawn aleatoire

--- obj/Debug/net8.0/BasicMonoGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/BasicMonoGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BasicMonoGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a33a2b2a7f4a645f58445235a5a6679b4c3c1d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0ea4a00cd8f8b01f5db046add00fcbce5b589a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BasicMonoGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BasicMonoGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
