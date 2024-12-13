52577fe29ccc0a2310bced674bb3c3a5fd0d7f7b
jon4t <jon4th4nrakotomalala@gmail.com>
2024-12-13 18:12:42 +0100

    Implementation Menu

--- Global.cs
+++ Global.cs
@@ -7,10 +7,15 @@ namespace BasicMonoGame;
 public class Global
 {
     public static float _Temps { get; set; }
+    
     public static SpriteBatch _spriteBatch;
     public static ContentManager _Content { get; set; }
-
-
+    
+    public static Game1 _game { get; set; }
+    
+    public static GraphicsDeviceManager _graphics { get; set; }
+    public static ScreenManager _ScreenManager { get; set; }
+    public static Screen _GameScreen { get; set; }
     public static void Update(GameTime gameTime)
     {
         _Temps = (float)gameTime.ElapsedGameTime.TotalSeconds;
--- obj/Debug/net8.0/BasicMonoGame.AssemblyInfo.cs
+++ obj/Debug/net8.0/BasicMonoGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BasicMonoGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0ea4a00cd8f8b01f5db046add00fcbce5b589a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cde7e5f4ab8931b19fc86928368487def2b9a9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("BasicMonoGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BasicMonoGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
