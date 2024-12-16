87bc4f48f89edef19bf673783715c915038b807d
Tianasoa Rasolonjatovo <rasolotianasoa@gmail.com>
2024-12-16 16:53:53 +0100

    update data

--- GameDataManager.cs
+++ GameDataManager.cs
@@ -1,6 +1,36 @@
+/*Cest moi qui a voulu faire ca*/
+
+
+/*using System;
+using System.IO;
+using System.Xml.Linq;
+
 namespace BasicMonoGame;
 
-public class GameDataManager
+public static class GameDataManager//creer un xml pour sauvegarder les données
 {
-    
-}
+    public static void SaveGameData(string playerName, int score, int enemiesKilled)
+    {
+        // creer une XML avec les informations du joueur
+        XElement gameData = new XElement("GameData",
+            new XElement("Player",
+                new XElement("Name", playerName),
+                new XElement("Score", score),
+                new XElement("EnemiesKilled", enemiesKilled),
+                new XElement("Date", DateTime.Now.ToString("yyyy-MM-dd"))
+            )
+        );
+
+        //  chemin du fichier XML dans LE dossier "Data"
+        string directoryPath = "Data";  // Répertoire où stocker les fichiers XML
+        
+
+        // Nom du fichier, basé sur la date pour qu'il soit unique
+        string filePath = Path.Combine(directoryPath, $"GameData_{DateTime.Now.ToString("yyyyMMdd_HHmmss")}.xml");
+
+        // Sauvegarder le fichier XML
+        gameData.Save(filePath);
+
+        Console.WriteLine($"Game data saved to {filePath}");
+    }
+}*/
--- Gamestat.cs
+++ Gamestat.cs
@@ -1,6 +1,63 @@
+/*Cest moi qui a voulu faire ca*/
+/*using Microsoft.Xna.Framework;
+
 namespace BasicMonoGame;
 
-public class Gamestat
+public class GameStats
 {
+    public static int Score { get; set; }
+    public static int EnemiesKilled { get; set; }
+
+
+
+    //initialisation des données
+    public static void ResetStats()
+    {
+        Score = 0;
+        EnemiesKilled = 0;
+    }
+
+    // Méthode qd le joueur tue un ennemi
+    public static void EnemyKilled(int points)
+    {
+        EnemiesKilled++;
+        Score += points;
+    }
+
+
+    public static void SaveGameData()
+    {
+        string playerName = "Player1";
+        int score = GameStats.Score;
+        int enemiesKilled = GameStats.EnemiesKilled;
+
+
+        // génère et sauvegarde les données en XML
+        GameDataManager.SaveGameData(playerName, score, enemiesKilled);
+    }
+
+    /*private void HandleGameOverSelection(GameTime gameTime)
+    {
+        switch (selectedIndex)
+        {
+            case 0: // Restart
+                // Réinitialiser les statistiques du jeu avant de redémarrer
+                GameStats.ResetStats();
+                Global._game.Content.Unload();
+                Global._ScreenManager.ChangeScreen(new InGameScreen());
+                break;
+
+            case 1: // Quit
+                // Sauvegarder les données du jeu avant de quitter
+                GameDataManager.SaveGameData();
+                Global._game.Content.Unload();
+                Global._game.GraphicsDevice.Clear(Color.CornflowerBlue);
+                Global._ScreenManager.ChangeScreen(new MainMenuScreen());
+                break;
+        }
+    }
+    }*///fin de la classe
+
     
-}
+
+    
